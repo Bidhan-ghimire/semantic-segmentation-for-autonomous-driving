31b84ac3bb5c7a1444c2caad53bad3a52c0fb011
Bidhan Ghimire <bidhanghimire23@gmail.com>
2026-01-28 21:12:12 -0500

    Update model codes

---
+++ src/models/deeplabv3plus.py
@@ -0,0 +1,127 @@
+import numpy as np
+from classification_models.tfkeras import Classifiers
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+import os
+
+
+def convolution_block(
+    block_input,
+    num_filters=256,
+    kernel_size=3,
+    dilation_rate=1,
+    padding="same",
+    use_bias=False,
+):
+    x = layers.Conv2D(
+        num_filters,
+        kernel_size=kernel_size,
+        dilation_rate=dilation_rate,
+        padding="same",
+        use_bias=use_bias,
+        kernel_initializer=keras.initializers.HeNormal(),
+    )(block_input)
+    x = layers.BatchNormalization()(x)
+    return tf.nn.relu(x)
+
+
+def DilatedSpatialPyramidPooling(dspp_input):
+    dims = dspp_input.shape
+
+    x = layers.AveragePooling2D(pool_size=(dims[-3], dims[-2]))(dspp_input)
+    x = convolution_block(x, kernel_size=1, use_bias=True)
+    out_pool = layers.UpSampling2D(
+        size=(dims[-3] // x.shape[1], dims[-2] // x.shape[2]), interpolation="bilinear",
+    )(x)
+
+    out_1 = convolution_block(dspp_input, kernel_size=1, dilation_rate=1)
+    out_6 = convolution_block(dspp_input, kernel_size=3, dilation_rate=6)
+    out_12 = convolution_block(dspp_input, kernel_size=3, dilation_rate=12)
+    out_18 = convolution_block(dspp_input, kernel_size=3, dilation_rate=18)
+
+    x = layers.Concatenate(axis=-1)([out_pool, out_1, out_6, out_12, out_18])
+    output = convolution_block(x, kernel_size=1)
+    return output
+
+
+
+def DeeplabV3PlusResnet18(image_size, num_classes):
+    model_input = keras.Input(shape=(image_size[0], image_size[1], 3))
+    
+    ResNet18, preprocess_input = Classifiers.get('resnet18')
+    resnet18 = ResNet18(weights='imagenet',include_top=False, input_tensor=model_input)
+    
+    x = resnet18.get_layer("relu1").output
+    x = DilatedSpatialPyramidPooling(x)
+
+
+    input_a = layers.UpSampling2D(
+        size=(image_size[0] // 4 // x.shape[1], image_size[1] // 4 // x.shape[2]),
+        interpolation="bilinear",
+    )(x)
+    
+    input_b = resnet18.get_layer("stage2_unit1_relu1").output
+    input_b = convolution_block(input_b, num_filters=48, kernel_size=1)
+    
+    input_a = tf.image.resize_with_crop_or_pad(input_a, input_b.shape[1], input_b.shape[2])
+    
+    x = layers.Concatenate(axis=-1)([input_a, input_b])
+    x = convolution_block(x)
+    x = convolution_block(x)
+    x = layers.UpSampling2D(
+        size=(image_size[0] // x.shape[1], image_size[1] // x.shape[2]),
+        interpolation="bilinear",
+    )(x)
+    model_output = layers.Conv2D(num_classes, kernel_size=(1, 1), padding="same",activation="softmax")(x)
+    
+    model = keras.Model(inputs=model_input, outputs=model_output)
+    
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False)
+    
+    model.compile(
+                 optimizer=keras.optimizers.Adam(learning_rate=0.001),
+                 loss=loss,
+                 metrics=["accuracy"])
+    return model
+
+
+def DeeplabV3PlusResnet50(image_size, num_classes):
+    model_input = keras.Input(shape=(image_size[0], image_size[1], 3))
+    
+    resnet50 = keras.applications.ResNet50(
+        weights="imagenet", include_top=False, input_tensor=model_input
+    )
+    
+    x = resnet50.get_layer("conv5_block3_out").output
+    x = DilatedSpatialPyramidPooling(x)
+
+
+    input_a = layers.UpSampling2D(
+        size=(image_size[0] // 4 // x.shape[1], image_size[1] // 4 // x.shape[2]),
+        interpolation="bilinear",
+    )(x)
+    
+    input_b = resnet50.get_layer("conv2_block3_out").output
+    input_b = convolution_block(input_b, num_filters=48, kernel_size=1)
+    
+    input_a = tf.image.resize_with_crop_or_pad(input_a, input_b.shape[1], input_b.shape[2])
+    
+    x = layers.Concatenate(axis=-1)([input_a, input_b])
+    x = convolution_block(x)
+    x = convolution_block(x)
+    x = layers.UpSampling2D(
+        size=(image_size[0] // x.shape[1], image_size[1] // x.shape[2]),
+        interpolation="bilinear",
+    )(x)
+    model_output = layers.Conv2D(num_classes, kernel_size=(1, 1), padding="same",activation="softmax")(x)
+    
+    model = keras.Model(inputs=model_input, outputs=model_output)
+    
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False)
+    
+    model.compile(
+                 optimizer=keras.optimizers.Adam(learning_rate=0.001),
+                 loss=loss,
+                 metrics=["accuracy"])
+    return model
